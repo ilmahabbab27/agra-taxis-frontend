2051b19b8fa4bcc7d0674fb5c4e8b366c03c6abc
Ilma <ilmahabbab27@gmail.com>
2026-09-21 16:52:32 +0530

    done

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,6 +1,6 @@
 export const PHONE = "+94723003000";
 export const PHONE_DISPLAY = "072 300 3000";
-export const WHATSAPP = "94702504044";
+export const WHATSAPP = "94723003000";
 export const EMAIL = "info@agrataxis.com";
 export const WA_DEFAULT_MESSAGE = "Hello Agra Taxis, I'd like a fare estimate.";
 
